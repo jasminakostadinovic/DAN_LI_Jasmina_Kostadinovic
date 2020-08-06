04776e388400b34b90972a5f83ba61b43c5c240a
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2020-08-06 12:19:22 +0200

    data validations added

---
+++ HealthcareData/Interfaces/IHealthcareValidations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareData.Interfaces
+{
+    public interface IHealthcareValidations
+    {
+        bool IsCorrectUser(string userName, string password);
+        bool IsUniqueUsername(string username);
+        bool IsUniquePersonalNo(string personalNo);
+        bool IsRequrementApproved(int requirementId);
+        bool IsDoctorChosen(int patientId);
+
+    }
+}
---
+++ HealthcareData/Validations/HealthcareValidations.cs
@@ -0,0 +1,100 @@
+﻿using HealthcareData.Interfaces;
+using HealthcareData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareData.Validations
+{
+    public class HealthcareValidations : IHealthcareValidations
+    {
+        public bool IsCorrectUser(string userName, string password)
+        {
+            try
+            {
+                using (var conn = new HealthcareAppDataEntities1())
+                {
+                    var user = conn.tblHealthcareUserDatas.FirstOrDefault(x => x.Username == userName && x.Password == password);
+                    if (user != null)
+                        return true;
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+        public bool IsDoctorChosen(int patientId)
+        {
+            try
+            {
+                using (var conn = new HealthcareAppDataEntities1())
+                {
+                    var patient = conn.tblPatients.FirstOrDefault(x => x.PatientID == patientId);
+                    if (patient != null)
+                        return patient.DoctorID != 0;
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool IsRequrementApproved(int requirementId)
+        {
+            try
+            {
+                using (var conn = new HealthcareAppDataEntities1())
+                {
+                    var requirement = conn.tblSickLeaveRequirements.FirstOrDefault(x => x.SickLeaveRequirementID == requirementId);
+                    if (requirement != null)
+                        return requirement.IsApproved;
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool IsUniquePersonalNo(string personalNo)
+        {
+            try
+            {
+                using (var conn = new HealthcareAppDataEntities1())
+                {
+                    return !conn.tblHealthcareUserDatas.Any(x => x.PersonalNo == personalNo);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool IsUniqueUsername(string username)
+        {
+            try
+            {
+                using (var conn = new HealthcareAppDataEntities1())
+                {
+                    return !conn.tblHealthcareUserDatas.Any(x => x.Username == username);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
